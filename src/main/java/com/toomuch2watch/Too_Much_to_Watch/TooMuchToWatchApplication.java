968fc2594376822deaf0c962c7f97aa11a72084b
RAlanWright <wright.alan88@gmail.com>
2021-03-21 16:18:15 -0500

    Set up initial Spring Boot directory structure

---
+++ src/main/java/com/toomuch2watch/Too_Much_to_Watch/TooMuchToWatchApplication.java
@@ -0,0 +1,13 @@
+package com.toomuch2watch.Too_Much_to_Watch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TooMuchToWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TooMuchToWatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toomuch2watch/Too_Much_to_Watch/TooMuchToWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toomuch2watch.Too_Much_to_Watch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TooMuchToWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
