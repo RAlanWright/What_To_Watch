62eab4b4e574d1ebf996b81c147198dd39455ca5
RAlanWright <wright.alan88@gmail.com>
2021-03-21 16:52:57 -0500

    Created getters and setters in Media and duration for Movie class

--- src/main/java/com/toomuch2watch/Too_Much_to_Watch/Media.java
+++ src/main/java/com/toomuch2watch/Too_Much_to_Watch/Media.java
@@ -12,4 +12,92 @@ abstract class Media {
     protected String rating;
     protected String genre;
     protected String description;
+
+    public String getShow_id() {
+        return show_id;
+    }
+
+    public void setShow_id(String show_id) {
+        this.show_id = show_id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getCast() {
+        return cast;
+    }
+
+    public void setCast(String cast) {
+        this.cast = cast;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getDate_added() {
+        return date_added;
+    }
+
+    public void setDate_added(String date_added) {
+        this.date_added = date_added;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- src/main/java/com/toomuch2watch/Too_Much_to_Watch/Movie.java
+++ src/main/java/com/toomuch2watch/Too_Much_to_Watch/Movie.java
@@ -1,4 +1,8 @@
 package com.toomuch2watch.Too_Much_to_Watch;
 
 public class Movie extends Media {
+    protected String duration;
+
+
+
 }
